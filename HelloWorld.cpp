5dd02000635d88914a7073537eeb3e4714ebd394
Ian Crowe-Sittig <ft5139@wayne.edu>
2024-09-10 20:11:56 -0400

    Adding author name

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,7 +1,11 @@
 // Simple C++ program to display "Hello World"
+// Author Ian Crowe-Sittig <ft5139@wayne.edu>
+
 // Header file for input output functions
 #include<iostream>
+
 using namespace std;
+
 // main function -
 // where the execution of program begins
 int main()
